a91d37cdaca624e4ffbba3472961cfc04f77b955
Robert Cortese <robert@robertcortese.com>
2020-12-23 10:17:51 -0800

    Create get-printlog.ps1

---
+++ get-printlog.ps1
@@ -0,0 +1,9 @@
+#Grabs a print log from a remote machine, with user that submitted the print job, date, etc.
+Get-WinEvent -computername somecomputername -FilterHashTable @{LogName="Microsoft-Windows-PrintService/Operational"; ID=307; StartTime=(Get-Date -OutVariable Now).AddDays(-1)} |
+Select-Object -Property TimeCreated,
+                        @{label='UserName';expression={$_.properties[2].value}},
+                        @{label='ComputerName';expression={$_.properties[3].value}},
+                        @{label='PrinterName';expression={$_.properties[4].value}},
+                        @{label='PrintSize';expression={$_.properties[6].value}},
+                        @{label='Pages';expression={$_.properties[7].value}} |
+Export-Csv -Path "Printing Audit - $($($Now).ToString('yyyy-MM-dd')).csv" -NoTypeInformation
