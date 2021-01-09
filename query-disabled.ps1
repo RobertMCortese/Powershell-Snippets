871cbf6c503c5c89496ec8e48a6cd4c18ead86f1
Robert Cortese <robert@robertcortese.com>
2021-01-09 11:28:08 -0800

    Update query-disabled.ps1

--- query-disabled.ps1
+++ query-disabled.ps1
@@ -1,4 +1,2 @@
-So yesterday a Tier3 team did a big goof at work.  We have a Microsoft Identity manager team that disabled about 561 accounts. At one point we were asked to ID the accounts.  I came up with this little bit of powershell that ID'd accounts modified in the last 24 and had an extensionattribute applied.  Saved the day.
-
 $When = ((Get-Date).AddDays(-1)).Date
 Get-ADUser -Filter {extensionAttribute4 -eq "Ourattribute" -and modified -ge $When} -Properties * -searchbase 'OU=oursupersecretou'| Select sAMAccountName, extensionAttribute4, modified | export-csv c:\oit\trainingdisable.csv -append
