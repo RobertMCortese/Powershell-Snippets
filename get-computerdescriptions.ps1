a6a58ae2c11e50ad053d84819fffa06ece6bb7f6
Robert Cortese <robert@robertcortese.com>
2021-05-12 16:06:35 -0700

    Update get-computerdescriptions.ps1

--- get-computerdescriptions.ps1
+++ get-computerdescriptions.ps1
@@ -1,21 +1,24 @@
-#Author robert.m.cortese
+#Author robert.cortese@va.gov
 #What does this do?
 #Get-Computerdescription takes a list of computernames and scans the c:\users directory for the most recently used profile
 #which is normally named after a samaccountname. After that, it will do a get-aduser on that samccount name, pulling
 #various bits of info on the user.  Info is then put into a CSV which can later be used with Set-adcomputer -description $description to 
 #fill in the computers description.
 
-$computers = gc C:\oit\ps1\desktops.txt
+$computers = gc C:\oit\livermore_filtered.txt
 ForEach ($computer in $computers){
 $lastuser = gci "\\$computer\c$\users" | sort LastWriteTime | select -last 1
+$ram = Get-WMIObject -computername $computer -class Win32_PhysicalMemory | Measure-Object -Property capacity -Sum | select @{N="TotalRam"; E={[math]::round(($_.Sum / 1GB),2)}}
+$makemodel = get-wmiobject -computername $computer win32_computersystem
+$OS = $(((gcim Win32_OperatingSystem -ComputerName $computer).Name).split('|')[0])
 $userinfo = get-aduser $lastuser.name -properties Name,telephoneNumber, Department  | select name, telephonenumber,department
-$description =$userinfo.name, $userinfo.telephonenumber, $userinfo.department 
-$separator = " "
+$description =$OS, "$($ram.totalram)GB RAM","$($makemodel.Manufacturer +" " + $makemodel.model)", "$(" Contact: " + $userinfo.name + " Phone:" + $userinfo.telephonenumber + " Department:"+$userinfo.department)"
+$separator = " | "
 $data = [pscustomobject]@{ 
 Computer = $Computer
 Description = [string]::Join($separator,$description)
 }
 
-$data | export-csv c:\oit\computerinfo-paldesktops.csv -append -NoTypeInformation
+$data | export-csv c:\oit\computerinfo-livermore.csv -append -NoTypeInformation
 $data 
 }
